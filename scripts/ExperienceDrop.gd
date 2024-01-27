3f33e33ac4ff94e5cb4aff00036b208dea3ad17b
Frank Weng <lpvvnnoobs@gmail.com>
2024-01-26 19:08:54 -0600

    Add pause menu

--- scripts/ExperienceDrop.gd
+++ scripts/ExperienceDrop.gd
@@ -6,5 +6,7 @@ func _ready():
 	get_parent().body_entered.connect(on_body_entered)
 
 func on_body_entered(body):
-	# apply exp to character
-	get_parent().queue_free()
+	var exp_component = body.find_child("Experience")
+	if exp_component:
+		exp_component.total_exp += exp_value
+		get_parent().queue_free()
