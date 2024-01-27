4aab21921414b090f61085211316ea83cdce267a
Luna Futures <luna.futures+github@gmail.com>
2024-01-27 03:29:00 -0600

    Check if additional_velocity_vector exists

--- scripts/LinearVelocity.gd
+++ scripts/LinearVelocity.gd
@@ -3,7 +3,12 @@ class_name LinearVelocity extends Node
 @export var velocity: float = 1000.0
 
 func _physics_process(delta):
+	var extra_vector = (get_parent().additional_velocity_vector * delta
+		if "additional_velocity_vector" in get_parent()
+		else Vector2.ZERO)
+	
 	get_parent().position += (
-		Vector2.from_angle(get_parent().rotation) * velocity * delta
-		+ get_parent().additional_velocity_vector * delta
+		Vector2.from_angle(
+			get_parent().rotation) * velocity * delta
+			+ extra_vector
 		)
