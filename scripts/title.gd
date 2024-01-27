7bfe638127009f02c25f2334486c2dcfe2fa9a23
unknown <aml2732@utexas.edu>
2024-01-27 13:41:54 -0600

    Return to title screen from pause menu

--- scripts/title.gd
+++ scripts/title.gd
@@ -4,10 +4,6 @@ func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://levels/survivors_level.tscn")
 
 
-func _on_continue_pressed():
-	get_tree().change_scene_to_file("res://levels/survivors_level.tscn")
-
-
 func _on_instructions_pressed():
 	get_tree().change_scene_to_file("res://levels/uipanels/instructions.tscn")
 
