5c184bcde1d65344fd0258d5b626072a9d950f4a
Frank Weng <lpvvnnoobs@gmail.com>
2024-01-26 23:05:34 -0600

    Split up on death behavior

---
+++ scripts/EnemyOnDeath.gd
@@ -0,0 +1,17 @@
+class_name EnemyOnDeath extends Node
+
+@export var spawn_on_death: PackedScene
+
+func _ready():
+	$"../HealthComponent".death.connect(on_death)
+
+func on_death():
+	if spawn_on_death:
+		var death_drop = spawn_on_death.instantiate()
+		death_drop.position = get_parent().position
+		Experience.add_child.call_deferred(death_drop)
+	var tween = create_tween().bind_node(self)
+	$"../CollisionShape2D".set_deferred("disabled", true)
+	tween.tween_method(func(val): $"../Sprite2D".material.set_shader_parameter("dissolve_value", val), 1.0, 0.0, 1.0)
+	tween.tween_callback(get_parent().queue_free)
+	
--- scripts/HealthComponent.gd
+++ scripts/HealthComponent.gd
@@ -1,6 +1,6 @@
 class_name HealthComponent extends ProgressBar
 
-@export var on_death: PackedScene
+signal death
 
 func _ready():
 	value = max_value
@@ -8,15 +8,6 @@ func _ready():
 	value_changed.connect(update)
 
 func update(_new_value):
-	visible = value < max_value
-	if value <= 0:
-		if on_death:
-			var death_drop = on_death.instantiate()
-			death_drop.position = get_parent().position
-			Experience.add_child.call_deferred(death_drop)
-		var tween = create_tween().bind_node(self)
-		visible = false
-		$"../CollisionShape2D".set_deferred("disabled", true)
-		tween.tween_method(func(val): $"../Sprite2D".material.set_shader_parameter("dissolve_value", val), 1.0, 0.0, 1.0)
-		tween.tween_callback(get_parent().queue_free)
-	
+	visible = value < max_value and value > 0.0
+	if value <= 0.0:
+		death.emit()
