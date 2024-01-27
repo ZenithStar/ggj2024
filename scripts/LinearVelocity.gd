ea4dbc1839b45976e61a0f4de8b42cc8a363694b
Luna Futures <luna.futures+github@gmail.com>
2024-01-27 02:47:11 -0600

    Make projectile speed relative to player speed

--- scripts/LinearVelocity.gd
+++ scripts/LinearVelocity.gd
@@ -3,4 +3,7 @@ class_name LinearVelocity extends Node
 @export var velocity: float = 1000.0
 
 func _physics_process(delta):
-	get_parent().position += Vector2.from_angle(get_parent().rotation) * velocity * delta
+	get_parent().position += (
+		Vector2.from_angle(get_parent().rotation) * velocity * delta
+		+ get_parent().additional_velocity_vector * delta
+		)
---
+++ scripts/placeholder_projectile.gd
@@ -0,0 +1,3 @@
+class_name PlaceholderProjectile extends Area2D
+
+@export var additional_velocity_vector = Vector2()
