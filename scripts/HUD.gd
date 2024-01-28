dc743fb6ce4be0a97cc0d5b0007ab602cacb3b59
Frank Weng <lpvvnnoobs@gmail.com>
2024-01-27 19:29:28 -0600

    Adding another tile floor and base for level up menu

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -1,15 +1,22 @@
 extends CanvasLayer
 
 var player
+var pause_menu
 var clock: Clock
 var experience: ExperienceAccumulator
+
 func _ready():
 	player = get_tree().root.find_child("Player", true, false)
+	pause_menu = get_tree().root.find_child("PauseMenuOverlay", true, false)
 	clock = player.find_child("Clock")
 	clock.timeout.connect(update_clock)
 	experience = player.find_child("ExperienceAccumulator")
 	experience.changed.connect(update_exp)
 	update_exp()
+	experience.level_up.connect(level_up)
+	$Control/VBoxContainer/LevelUp/MarginContainer/HBoxContainer/TextureButton.pressed.connect(resume)
+	$Control/VBoxContainer/LevelUp/MarginContainer/HBoxContainer/TextureButton2.pressed.connect(resume)
+	$Control/VBoxContainer/LevelUp/MarginContainer/HBoxContainer/TextureButton3.pressed.connect(resume)
 
 func update_clock():
 	$Control/VBoxContainer/PanelContainer/ClockLabel.text = clock.time_string()
@@ -17,3 +24,14 @@ func update_clock():
 func update_exp():
 	$Control/VBoxContainer/ProgressBar.value = experience.current_exp / experience.get_exp_to_level() * 100.0
 	$Control/VBoxContainer/ProgressBar/LevelLabel.text = "Level: %d "%[experience.level+1]
+
+func level_up(_level:int):
+	get_tree().paused = true
+	pause_menu.enabled = false
+	GuiTransitions.show("level_up_options")
+	$Control/VBoxContainer/LevelUp/MarginContainer/HBoxContainer/TextureButton.grab_focus()
+
+func resume():
+	GuiTransitions.hide("level_up_options")
+	get_tree().paused = false
+	pause_menu.enabled = true
