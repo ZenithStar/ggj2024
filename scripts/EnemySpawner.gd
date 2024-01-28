5633d7e902c5c0fc55d4c9b5a8c9db151dc33fc2
Luna Futures <luna.futures+github@gmail.com>
2024-01-28 15:45:45 -0600

    Change enemy count by time elapsed

--- scripts/EnemySpawner.gd
+++ scripts/EnemySpawner.gd
@@ -2,7 +2,6 @@ class_name EnemySpawner extends Timer
 
 @export var enemy_scene: PackedScene = preload("res://prefabs/placeholder_enemy.tscn")
 @export var enemy_scene2: PackedScene = preload("res://prefabs/placeholder_enemy2.tscn")
-@export var max_enemies: int = 100
 var dialogue_bubble_swear: PackedScene = preload("res://prefabs/dialogue_bubble_swear.tscn")
 var dialogue_bubble_text: PackedScene = preload("res://prefabs/dialogue_bubble_variable.tscn")
 
@@ -30,14 +29,47 @@ func instantiate_base_karen():
 			text_bubble.scale = Vector2(0.3, 0.3)
 			temp_enemy.add_child(text_bubble)
 	return temp_enemy
+	
+func get_elapsed_seconds():
+	return $"../Clock".elapsed()
+	
+func get_max_enemies():
+	var sec = get_elapsed_seconds()
+	if sec <= 30:
+		return 10
+	elif sec <= 60:
+		return 20
+	elif sec <= 90:
+		return 35
+	else:
+		return 50
+	
+func get_easier_enemy_chance():
+	var sec = get_elapsed_seconds()
+	if sec <= 40:
+		return 101
+	elif sec <= 60:
+		return 99
+	elif sec <= 90:
+		return 95
+	else:
+		return 90
 
 func spawn():
-	if randi_range(0,max_enemies) > get_child_count():
-		var new_enemy = instantiate_base_karen() if true_some_percent_of_time(95) else enemy_scene2.instantiate()
-		new_enemy.find_child("TargetManager").target = get_parent()
-		new_enemy.position = Vector2.from_angle(randf_range(-PI,PI)) * new_enemy.find_child("LimitDistanceToTarget").spawn_distance + get_parent().global_position
-		if new_enemy.find_child("Dialog"):
-			new_enemy.find_child("Dialog").position = new_enemy.position + Vector2(-30, 0)
-		if new_enemy.find_child("GenericShadow"):
-			new_enemy.find_child("GenericShadow").position = new_enemy.position + Vector2(-10, -10)
-		add_child(new_enemy)
+	if get_child_count() > get_max_enemies():
+		return
+		
+	var new_enemy = (
+		instantiate_base_karen() 
+		if true_some_percent_of_time(get_easier_enemy_chance()) 
+		else enemy_scene2.instantiate())
+	
+	new_enemy.find_child("TargetManager").target = get_parent()
+	new_enemy.position = Vector2.from_angle(randf_range(-PI,PI)) * new_enemy.find_child("LimitDistanceToTarget").spawn_distance + get_parent().global_position
+	
+	if new_enemy.find_child("Dialog"):
+		new_enemy.find_child("Dialog").position = new_enemy.position + Vector2(-30, 0)
+	if new_enemy.find_child("GenericShadow"):
+		new_enemy.find_child("GenericShadow").position = new_enemy.position + Vector2(-10, -10)
+	
+	add_child(new_enemy)
