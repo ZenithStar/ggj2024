d97b3813cc2ba249e95d5c1ebe9da13d1dc78bd1
Frank Weng <lpvvnnoobs@gmail.com>
2024-01-28 14:13:46 -0600

    Added a container to correctly scale upgrade icons

--- prefabs/upgrade_selection.gd
+++ prefabs/upgrade_selection.gd
@@ -6,6 +6,6 @@ extends TextureButton
 		var data = Upgrades.get_data(property_name)
 		$MarginContainer/HFlowContainer/Name.text = data["flavor_name"]
 		$MarginContainer/HFlowContainer/Level.text = "Level %s"%[Upgrades.get_next_level(property_name)]
-		$MarginContainer/HFlowContainer/Icon.texture = data["icon"]
+		$MarginContainer/HFlowContainer/PanelContainer/Icon.texture = data["icon"]
 		$MarginContainer/HFlowContainer/Tooltip.text = data["tooltip"]
 		$MarginContainer/HFlowContainer/Flavor.text = data["flavor_text"]
