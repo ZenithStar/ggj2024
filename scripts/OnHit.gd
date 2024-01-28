477afd1d569d6733088b018c9d4e21def7a42c1b
Luna Futures <luna.futures+github@gmail.com>
2024-01-28 04:31:12 -0600

    Add knockback ability for projectiles

--- scripts/OnHit.gd
+++ scripts/OnHit.gd
@@ -3,6 +3,7 @@ class_name OnHit extends Node
 @export var floating_damage: PackedScene = preload("res://prefabs/floating_damage.tscn")
 @export var damage_min_range: float = 70.0
 @export var damage_max_range: float = 100.0
+@export var knockback_distance: float = 25
 
 func _ready():
 	get_parent().body_entered.connect(on_hit)
@@ -10,10 +11,20 @@ func _ready():
 
 func on_hit(object):
 	var health: HealthComponent = object.find_child("HealthComponent")
-	if health:
-		var damage = randf_range(damage_min_range, damage_max_range)
-		health.value -= damage
-		var floater = floating_damage.instantiate()
-		floater.set_text("%d"%[int(damage)])
-		floater.position = get_parent().global_position
-		get_parent().add_sibling(floater)
+	if not health:
+		return
+
+	var damage = randf_range(damage_min_range, damage_max_range)
+	health.value -= damage
+	var floater = floating_damage.instantiate()
+	floater.set_text("%d"%[int(damage)])
+	floater.position = get_parent().global_position
+	get_parent().add_sibling(floater)
+	
+	# Knockback
+	var rotation = get_parent().rotation
+	var knockback_vector = Vector2.from_angle(rotation) * knockback_distance
+	var before = object.global_position
+	object.global_position = object.global_position + knockback_vector
+	var after = object.global_position
+	prints(before, knockback_vector, after)
