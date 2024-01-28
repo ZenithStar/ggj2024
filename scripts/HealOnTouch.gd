677b34f32a45d8824ab83faf631a3d0f6217d6d0
Luna Futures <luna.futures+github@gmail.com>
2024-01-27 23:20:35 -0600

    Add gulp sound when drinking liquor

--- scripts/HealOnTouch.gd
+++ scripts/HealOnTouch.gd
@@ -5,6 +5,9 @@ class_name HealOnTouch extends Node
 func _physics_process(delta):
 	for body in get_parent().get_overlapping_bodies():
 		var health = body.find_child("HealthComponent")
+		
 		if health:
 			health.value += healedHP
-			get_parent().queue_free()
+			get_parent().find_child("GulpSound").play()
+			get_parent().visible = false
+			get_tree().create_timer(1).timeout.connect(get_parent().queue_free)
