7ca79e33963fe8a2dc832a8f29b8923c476c9c07
Frank Weng <lpvvnnoobs@gmail.com>
2024-01-28 12:43:47 -0600

    Adjusting the enemy2 rate of fire and fixed a title screen error

--- scripts/title.gd
+++ scripts/title.gd
@@ -7,11 +7,12 @@ func _ready():
 	var hack = blood.instantiate()
 	hack.position = get_viewport().get_visible_rect().size
 	add_child.call_deferred(hack)
+	$"MarginContainer/VBoxContainer/SplitScreen/ButtonContainer/New Game".grab_focus()
 
 func _notification(what):
 	match what:
 		NOTIFICATION_VISIBILITY_CHANGED:
-			if visible:
+			if visible and $"MarginContainer/VBoxContainer/SplitScreen/ButtonContainer/New Game".is_inside_tree():
 				$"MarginContainer/VBoxContainer/SplitScreen/ButtonContainer/New Game".grab_focus()
 				
 func _on_new_game_pressed():
